1498428d9ca1b50e53e938e246ef9d56a2c3fcef
jja <jja@DESKTOP-M96DH6I>
2022-11-26 11:26:06 +0900

    첫 커밋.

---
+++ MyWebServer/src/test/java/caferoxie/dm/MyWebServerApplicationTests.java
@@ -0,0 +1,13 @@
+package caferoxie.dm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyWebServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
